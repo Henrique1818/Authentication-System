ab84ddabe3a78026808065753a722a8424186e62
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-27 22:18:30 -0300

    :construction: refactoring the app - IN POO

--- src/app.js
+++ src/app.js
@@ -1,28 +1,48 @@
-const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const router = require('./routes/User');
+const express = require('express');
+const routes = require('./routes/User');
+// const router = require('./routes/User');
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.use(router);
+// app.use(router);
 
-app.use((req, res, next) => {
-    const error = new Error('Não encontrado');
-    error.status = 404
+// app.use((req, res, next) => {
+//     const error = new Error('Não encontrado');
+//     error.status = 404
 
-    next(error);
-});
+//     next(error);
+// });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
-});
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     return res.send({
+//         erro: {
+//             mensagem: error.message
+//         }
+//     });
+// });
 
-module.exports = app;
+// module.exports = app;
+
+
+class AppController {
+    constructor() {
+        this.express = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.express.use(express.json());
+        this.express.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+    routes() {
+        this.express.use(routes);
+    }
+}
+
+module.exports = new AppController().express;
