2aa3cb5b7e9ad80669752d64cc9e761491ca3765
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-27 21:52:04 -0300

    :contruction: refactoring the database connection

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
-const url = 'url-bancoDeDados';
+const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+console.log(url)
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
