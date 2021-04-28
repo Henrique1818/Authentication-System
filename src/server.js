e196819caab64be04d77211faae199c770423430
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-27 22:18:04 -0300

    :construction: refactoring the server -IN POO

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
-const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || port);
+app.listen(process.env.PORT || 3000);
