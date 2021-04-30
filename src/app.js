34058da5b8c050cae8354185d48997acb49e047a
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-29 22:30:46 -0300

    refatorando app

--- src/app.js
+++ src/app.js
@@ -1,32 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const routes = require('./routes/User');
-// const router = require('./routes/User');
-
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// app.use(router);
-
-// app.use((req, res, next) => {
-//     const error = new Error('Não encontrado');
-//     error.status = 404
-
-//     next(error);
-// });
-
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     return res.send({
-//         erro: {
-//             mensagem: error.message
-//         }
-//     });
-// });
-
-// module.exports = app;
-
 
 class AppController {
     constructor() {
