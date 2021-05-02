93d104189eaade99b36e686206bd5d485fc16e6c
Henrique1818 <luiz.henrique01827@gmail.com>
2021-05-01 23:21:20 -0300

    fazendo pequenas alteraçoes no Database

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-console.log(url)
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
