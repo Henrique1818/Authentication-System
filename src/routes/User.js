135e625f94269b787b7cf8f24a95b554527c3850
Henrique1818 <luiz.henrique01827@gmail.com>
2021-05-01 23:21:39 -0300

    Refatorando a rota de login

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 
-const User = require('../app/controllers/User');
+const UserSvc = require('../app/controllers/User');
 const Auth = require('../app/middlewares/Auth');
 
 const routes = Router();
 
-routes.get('/singin', User.login);
-routes.get('/:id', Auth, User.seekingUser)
-routes.post('/singup', User.create);
-routes.put('/:email', User.update);
+routes.get('/singin', UserSvc.getLogin);
+// routes.get('/:id', Auth, User.seekingUser)
+routes.post('/singup', UserSvc.createUser);
+// routes.put('/:email', User.update);
 
 module.exports = routes;
