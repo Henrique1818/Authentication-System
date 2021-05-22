c349f449c583a2e4eb64017135d06d1f3f897811
Henrique1818 <luiz.henrique01827@gmail.com>
2021-05-22 18:39:13 -0300

    refatorando login e a buscar de usuario

--- src/app/controllers/User.js
+++ src/app/controllers/User.js
@@ -10,43 +10,68 @@ function generateToken(params = {}) {
 };
 
 class UserSvc {
-    async createUser(req, res) {
+    async create(req, res) {
         try {
             const { email } = req.body;
-            let user = await User.find({ email }).exec();
+            let user = await User.find({ email }).lean();
             
             const newUser = User.create({ ...req.body, token: generateToken({ id: user._id }) });
-            
+
             return res.status(201).json(await newUser);
         } catch (err) {
             return res.status(400).json({ error: `Registration failed, ${err}` });
         }
     }
 
-    // async getLogin(req, res) {
-    //     try {
-    //         const { email, senha } = req.body;
-    //         const current = new Date().toLocaleString();
+    async getLogin(req, res) {
+        try {
+            const { email, senha } = req.body;
+            const current = new Date();
 
-    //         let user = await User.findOne({ email }).select('+senha');
+            let user = await User.findOne({ email }).select('+senha').lean();
 
-    //         if(!user || !await bcrypt.compare(senha, user.senha)) return res.status(403).json({ error: 'Invalid username and / or password' });
+            if (!user || !await bcrypt.compare(senha, user.senha)) return res.status(403).json({ error: 'Invalid username and / or password' });
 
-    //         await user.updateOne({
-    //             ...user,
-    //             ultimoLogin: current,
-    //             token: generateToken({ id: user._id })
-    //         }, { new: false, upsert: true });
+            user = await User.findOneAndUpdate({ email }, {
+                ...user,
+                ultimoLogin: current,
+                token: generateToken({ id: user._id })
+            }, { new: true, upsert: true }).lean();
 
-    //         user = await User.findOne({ email }).select('+senha');
-    //         user.senha = undefined;
+            user.senha = undefined;
 
-    //         return res.status(200).json({ user });
+            return res.status(200).json(user);
 
-    //     } catch(err) {
-    //         return res.status(400).json({ error: 'Login failed'});
-    //     }
-    // }
+        } catch (err) {
+            return res.status(400).json({ error: `Login failed, ${err}` });
+        }
+    }
+
+    async searchForUser(req, res) {
+        try {
+            const { id } = req.params;
+            let [, token ] = req.headers.authorization.split(' ');
+
+            let user = await User.findOne({_id: id}).lean();
+            
+            // 30min
+            let limitTime = Math.round((Date.now() - user.ultimoLogin) / (1000 * 60));
+
+            const verifyToken = user.token === token;
+
+            if (!verifyToken) return res.status(403).json({ error: 'Not authorized'});
+
+            switch(verifyToken) {
+                case limitTime <= 30: 
+                    return res.status(200).json(user);
+                default: 
+                    return res.status(401).json({ error: 'Invalid session'});
+            }
+        }
+        catch(err) {
+            return res.status(400).json({ error: 'Search failed'})
+        }
+    }
 
     // async update(req, res) {
     //     try {
